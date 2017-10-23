27cfafb05a36bd4e8634835ff0554ea25fe8910d
talgatv <talgat.v1994@gmail.com>
2017-10-23 20:40:51 +0500

    создаю webapp

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ DJANGO_APPS = [
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
 
     # Useful template tags:
     # 'django.contrib.humanize',
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
+class Client(models.Model):
+    name.CharField
+    last_name.CharField
+    given_name.CharField
+    record_number
+    insurance_number.CharField
+    phone.CharField
+    adress.CharField
+
+class Record(models.Model):
+    date.DateField
+    client_id.CharField
+    complains.TextField
+    life_and_medical_history.TextField
+    physical_exam_results.TextField
+    diagnosis.TextField
+    test_result.TextField
+    differential_diagnosis.TextField
+    
+
+class Personnel(models.Model):
+    name.CharField
+    last_name.CharField
+    given_name.CharField
+    speciality forgen key speciality
+    hospital
+class Scpeciality(models.Model):
+    surgery.CharField
+    general_practitioner.CharField
+    nurse.CharField
+    other.CharField
+
+
 # Create your models here.
